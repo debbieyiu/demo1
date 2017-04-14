98836f3afd060e8fa41e87163c43595d902db73c
Debbie Yiu <debbie.yiu.0219@gmail.com>
2017-04-14 11:12:33 +0800

    05 練習 Angular 資料繫結方法：內嵌繫結、屬性繫結

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,3 +1,4 @@
+import { Title } from '@angular/platform-browser/platform-browser';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  title = "The Deb Deb Web";
   constructor() { }
 
   ngOnInit() {
